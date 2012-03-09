04a8909cd86738d50584b45732ec6b58392b0d61
Armag3ddon <Armag3ddon@web.de>
2012-03-09 13:10:20 +0000

    dinge, die geschehen

--- steampunkzombies/src/potjam/map/World.java
+++ steampunkzombies/src/potjam/map/World.java
@@ -50,13 +50,8 @@ public class World {
 		// Earth & Grass
 		textures.put("EarthBack", new Image("ressources/map/EarthBack.png"));
 		textures.put("GrassOverlay", new Image("ressources/map/GrassOverlay.png"));
-		textures.put("EarthTopGrass", new Image("ressources/map/EarthTopGrass.png"));
-		textures.put("EarthBack", new Image("ressources/map/EarthBack.png"));
-		textures.put("EarthPlatformCornerLeftOverlay", new Image("ressources/map/EarthPlatformCornerLeftOverlay.png"));
-		textures.put("EarthPlatformCornerRightOverlay", new Image("ressources/map/EarthPlatformCornerRightOverlay.png"));
-		textures.put("EarthPlatformSingle", new Image("ressources/map/EarthPlatformSingle.png"));
-		textures.put("EarthPlatformSingleCornerLeft", new Image("ressources/map/EarthPlatformSingleCornerLeft.png"));
-		textures.put("EarthPlatformSingleCornerRight", new Image("ressources/map/EarthPlatformSingleCornerRight.png"));
+		textures.put("GrassOverlayLeft", new Image("ressources/map/GrassOverlayLeft.png"));
+		textures.put("GrassOverlayRight", new Image("ressources/map/GrassOverlayRight.png"));
 		textures.put("EarthShadowOverlay", new Image("ressources/map/EarthShadowOverlay.png"));
 		textures.put("EarthTopBrick", new Image("ressources/map/EarthTopBrick.png"));
 		// Brick
@@ -86,29 +81,39 @@ public class World {
 	 */
 	private static void loadMap() throws SlickException {
 		//Zombies - Breite und Hoehe bitte immer gleich lassen
-		/*int zombieWidth = 48;
+		int zombieWidth = 48;
 		int zombieHeight = 73;
-		
-		Zombie z = new Zombie(300, 100, zombieWidth, zombieHeight);
-		zombies.add(z);
-		
-		z = new Zombie(700, 200, zombieWidth, zombieHeight);
-		zombies.add(z);*/
-
 
 		//Bloecke
-		createForeground(-1000,-400, 1000,1000, "Void");
-		createStandardEarth(0,276, 1000,500);
-		createEarthSinglePlatform(400,100, 100, false);
-		createEarthSinglePlatform(700,-80, 150, false);
-		createForeground(1000,0, 150,276, "Rock");
-		createForeground(1000,276, 150,500, "");
-		createStandardEarth(1150,276, 150,500);
-		createStandardEarth(1300,314, 150,462);
-		createStandardEarth(1450,352, 150,424);
-		createStandardEarth(1600,390, 500,376);
-		createBackground(1300,0, 150,314, "EarthBack");
-		createBackground(1450,0, 150,276, "EarthBack");
+		float map_x = -1000; // dient dazu, mir Berechnungen beim Erstellen von Blöcken von links nach rechts zu ersparen
+		
+		createForeground(map_x,-400, 1000,1000, "Void");
+		map_x += 1000;
+
+		createStandardEarth(map_x,276, 1024,512);
+		createEarthPlatform(400,100, 100, 32, false);
+		createEarthPlatform(700,-80, 150, 32, false);
+		map_x += 1024;
+		Zombie z = new Zombie(map_x-60, 190, zombieWidth, zombieHeight);
+		zombies.add(z);
+		
+		createForeground(map_x,0, 128,276, "Rock");
+		createForeground(map_x,276, 128,512, "");
+		map_x += 128;
+		
+		createStandardEarth(map_x,276, 128,512);
+		map_x += 128;
+		
+		createStandardEarth(map_x,308, 128,470);
+		createBackground(map_x,0, 128,308, "EarthBack");
+		createEarthPlatform(map_x,-32, 256,32, true);
+		map_x += 128;
+		
+		createStandardEarth(map_x,340, 128,438);
+		createBackground(map_x,0, 128,340, "EarthBack");
+		map_x += 128;
+		
+		createStandardEarth(map_x,372, 512,406);
 		
 	}
 	// Erzeugt einen Vordergrundbereich
@@ -119,31 +124,33 @@ public class World {
 		tiles.add(b);
 	}
 	// Erzeugt einen Hintergrundbereich
-		private static void createBackground(float x, float y, float width, float height, String texture) throws SlickException {
-			Block b = new Block(x, y, width, height);
-			if (texture != "")
-				b.setTexture(textures.get(texture));
-			backgroundTiles.add(b);
-		}
-	// Setzt 38 Pixel höher als y an, um das Gras oben drauf zu zeichnen
-	private static void createStandardEarth(float x, float y, float width, float height) throws SlickException {
-		createBackground(x, y-38, width, 38, "GrassOverlay");
-		createForeground(x, y, width, 38, "EarthTopGrass");
-		createForeground(x, y+38, width, height-38, "");
+	private static void createBackground(float x, float y, float width, float height, String texture) throws SlickException {
+		Block b = new Block(x, y, width, height);
+		if (texture != "")
+			b.setTexture(textures.get(texture));
+		backgroundTiles.add(b);
 	}
-	// Einzeilige Erdplattform mit Breite width
-	// Macht in alle Richtungen Extras (46 links/rechts, 38 oben/unten)
-	// Minimumbreite 2*46
-	private static void createEarthSinglePlatform(float x, float y, float width, boolean shadow) throws SlickException {
-		if (width > 92)
-			createForeground(x+46, y, width-92, 38, "EarthPlatformSingle");
-		createForeground(x, y, 46, 38, "EarthPlatformSingleCornerLeft");
-		createForeground(x+width-46, y, 46, 38, "EarthPlatformSingleCornerRight");
-		createBackground(x, y-38, width, 38, "GrassOverlay");
-		createBackground(x-46, y, 46, 38, "EarthPlatformCornerLeftOverlay");
-		createBackground(x+width, y, 46, 38, "EarthPlatformCornerRightOverlay");
+	// Erzeugt einen Nicht-Kollisions-Vordergrundbereich
+	private static void createOverlay(float x, float y, float width, float height, String texture) throws SlickException {
+		Block b = new Block(x, y, width, height);
+		if (texture != "")
+			b.setTexture(textures.get(texture));
+		foregroundTiles.add(b);
+	}
+	// Setzt 4 Pixel höher als y an, um das Gras oben drauf zu zeichnen
+	private static void createStandardEarth(float x, float y, float width, float height) throws SlickException {
+		createForeground(x, y, width, height, "");
+		createOverlay(x, y-4, width, 14, "GrassOverlay");
+	}
+	// Erdplattform
+	// Macht in alle Richtungen Extras (46 links/rechts, 4 oben, 12 unten)
+	private static void createEarthPlatform(float x, float y, float width, float height, boolean shadow) throws SlickException {
+		createForeground(x, y, width, height, "");
+		createOverlay(x, y-4, width, 14, "GrassOverlay");
+		createOverlay(x-4, y, 15, height, "GrassOverlayLeft");
+		createOverlay(x+width-12, y, 15, height, "GrassOverlayRight");
 		if (shadow)
-			createBackground(x, y+38, width, 38, "EarthShadowOverlay");
+			createOverlay(x, y+height, width, 13, "EarthShadowOverlay");
 	}
 
 	public static void update(GameContainer gc, int delta) throws SlickException {
